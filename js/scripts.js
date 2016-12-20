bf253e2c53e994382b025d2719b55fb25cf049a2
Brent Perry <burentoperii@gmail.com>
2016-12-20 17:58:26 -0600

    complete encryption.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 var square = 1;
 var arrays = [];
+var encryption = "";
 
 var findSquare = function(message) {
   if (square * square < message.length) {
@@ -19,17 +20,30 @@ var makeArrays = function(message, squareSize) {
   return arrays;
 }
 
+var assembleArrays = function() {
+  for (var index = 0; index < arrays.length; index += 1) {
+    for (var index2 = 0; index2 < arrays[index].length; index2 += 1) {
+      encryption += arrays[index][index2];
+      if (encryption.replace(/[ ]/g, '').length % 5 === 0) {
+        encryption += " ";
+      }
+    }
+  }
+}
+
 var encryptMessage = function(message) {
   var noPunctuation = message.replace(/[ .,?!;:]/g, '').toLowerCase();
   squareSize = findSquare(noPunctuation);
-  return makeArrays(noPunctuation, squareSize);
+  makeArrays(noPunctuation, squareSize);
+  assembleArrays();
+  return encryption;
 }
 
 $(document).ready(function() {
   $("form#encryption").submit(function(event) {
     event.preventDefault();
     var message = $("#message").val();
-    var encryption = encryptMessage(message);
+    encryption = encryptMessage(message);
     $("#output-original").text(message);
     $("#output-encrypted").text(encryption);
     $("#result").show();
